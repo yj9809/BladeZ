df38623086a532c954dca0978bcf07241298c982
jiung200 <112762331+jiung200@users.noreply.github.com>
2026-05-11 00:42:41 +0900

    Feat: Add 폭발 액터 기본 로직 추가
    -폭발 드럼통 로직 추가

---
+++ Source/BladeZ/Interactable/BZExplosiveBarrel.cpp
@@ -0,0 +1,129 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactable/BZExplosiveBarrel.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystem.h"
+#include "Sound/SoundBase.h"
+
+ABZExplosiveBarrel::ABZExplosiveBarrel()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	Mesh->SetSimulatePhysics(true);
+	Mesh->SetCollisionProfileName(TEXT("PhysicsActor"));
+	
+	// OnHit 바인딩
+	Mesh->OnComponentHit.AddDynamic(this, &ABZExplosiveBarrel::OnHit);
+
+	CurrentHealth = MaxHealth;
+}
+
+void ABZExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABZExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABZExplosiveBarrel::ReceiveDamage_Implementation(float DamageAmount, AActor* DamageCauser)
+{
+	if (bHasExploded) return;
+
+	CurrentHealth -= DamageAmount;
+	if (CurrentHealth <= 0.0f)
+	{
+		Explode();
+	}
+}
+
+void ABZExplosiveBarrel::Grab(USceneComponent* ParentComponent, FName SocketName)
+{
+	if (bHasExploded || !ParentComponent) return;
+
+	bIsGrabbed = true;
+	bIsThrown = false;
+
+	// 물리 시뮬레이션 중단 및 충돌 설정 변경 (보스에게 붙어야 하므로)
+	Mesh->SetSimulatePhysics(false);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	// 컴포넌트에 부착
+	AttachToComponent(ParentComponent, FAttachmentTransformRules::SnapToTargetNotIncludingScale, SocketName);
+}
+
+void ABZExplosiveBarrel::Throw(const FVector& ThrowVelocity)
+{
+	if (bHasExploded || !bIsGrabbed) return;
+
+	bIsGrabbed = false;
+	bIsThrown = true;
+
+	// 부착 해제
+	DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+
+	// 물리 시뮬레이션 재개 및 충돌 활성화
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Mesh->SetSimulatePhysics(true);
+
+	// 투척 속도 적용
+	Mesh->SetPhysicsLinearVelocity(ThrowVelocity);
+}
+
+void ABZExplosiveBarrel::Explode()
+{
+	if (bHasExploded) return;
+	bHasExploded = true;
+
+	// 시각/청각 효과
+	if (ExplosionEffect)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+	}
+	if (ExplosionSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ExplosionSound, GetActorLocation());
+	}
+
+	// 주변 데미지 처리
+	TArray<AActor*> IgnoreActors;
+	IgnoreActors.Add(this);
+	UGameplayStatics::ApplyRadialDamage(
+		this,
+		ExplosionDamage,
+		GetActorLocation(),
+		ExplosionRadius,
+		UDamageType::StaticClass(),
+		IgnoreActors,
+		this,
+		GetInstigatorController(),
+		true
+	);
+
+	// 드럼통 제거
+	Destroy();
+}
+
+void ABZExplosiveBarrel::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// 필요 시 구현
+}
+
+void ABZExplosiveBarrel::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// 던져진 상태에서 무언가에 부딪히면 폭발
+	if (bIsThrown && !bHasExploded)
+	{
+		// 자기 자신이나 보스(던진 사람)가 아닌 경우에만 폭발하게 하려면 추가 체크 필요
+		// 여기서는 일단 부딪히면 폭발하도록 설정
+		Explode();
+	}
+}
---
+++ Source/BladeZ/Interactable/BZExplosiveBarrel.h
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/BZDamageable.h"
+#include "BZExplosiveBarrel.generated.h"
+
+/**
+ * 평소에 맵에 배치되어 있다가 보스가 집어서 던질 수 있는 폭발용 드럼통
+ */
+UCLASS()
+class BLADEZ_API ABZExplosiveBarrel : public AActor, public IBZDamageable
+{
+	GENERATED_BODY()
+	
+public:	
+	ABZExplosiveBarrel();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	// IBZDamageable Interface
+	virtual void ReceiveDamage_Implementation(float DamageAmount, AActor* DamageCauser) override;
+
+	// 보스가 집을 때 호출
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	void Grab(USceneComponent* ParentComponent, FName SocketName);
+
+	// 보스가 던질 때 호출
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	void Throw(const FVector& ThrowVelocity);
+
+	// 폭발 처리
+	UFUNCTION(BlueprintCallable, Category = "Interaction")
+	void Explode();
+
+protected:
+	// 충돌 시 폭발 체크를 위한 함수
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	TObjectPtr<class UStaticMeshComponent> Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat")
+	float MaxHealth = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat")
+	float CurrentHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat")
+	float ExplosionDamage = 50.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stat")
+	float ExplosionRadius = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effect")
+	TObjectPtr<class UParticleSystem> ExplosionEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Effect")
+	TObjectPtr<class USoundBase> ExplosionSound;
+
+private:
+	bool bIsGrabbed = false;
+	bool bHasExploded = false;
+	bool bIsThrown = false;
+};
