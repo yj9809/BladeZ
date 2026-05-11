dd48e270792f8eeb48d2622e108ba033edc1f1a9
SuYeon <includeashe@gmail.com>
2026-05-11 11:03:12 +0900

    Chore: Component 폴더 구조 변경
    [Add]
    - BZCharacterStatData

---
+++ Source/BladeZ/GameData/BZCharacterStat.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "BZCharacterStat.generated.h"
+
+// 데이터 테이블을 임포트할 때 행(Row) 데이터를 정의하는 구조체.
+// Row-Data = Record(레코드).
+USTRUCT(BlueprintType)
+struct FBZCharacterStat : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	FBZCharacterStat()
+		: MaxHp(0.0f),
+		AttackDamage(0.0f)
+	{
+	}
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stat)
+	float MaxHp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stat)
+	float AttackDamage;
+};
