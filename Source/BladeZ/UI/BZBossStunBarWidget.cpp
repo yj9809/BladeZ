3a1020b79671ef898b79d632d0cdbf3ef96ec016
SuYeon <includeashe@gmail.com>
2026-05-19 22:05:56 +0900

    Design: 빛나는 UI Material 추가

---
+++ Source/BladeZ/UI/BZBossStunBarWidget.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/BZBossStunBarWidget.h"
+#include "Components/ProgressBar.h"
+
+UBZBossStunBarWidget::UBZBossStunBarWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UBZBossStunBarWidget::UpdateProgress(float InProgress, bool InLightOn)
+{
+	Pb_Stun->SetPercent(InProgress);
+
+	SetLight(InLightOn);
+}
+
+void UBZBossStunBarWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// 제대로 Bind됐는지 확인.
+	ensureAlways(Pb_Stun);
+}
+
+void UBZBossStunBarWidget::SetLight(bool InLight)
+{
+	//Light 설정은 해야됨.
+}
---
+++ Source/BladeZ/UI/BZBossStunBarWidget.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BZUserWidget.h"
+#include "BZBossStunBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLADEZ_API UBZBossStunBarWidget : public UBZUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UBZBossStunBarWidget(const FObjectInitializer& ObjectInitializer);
+
+	// Light 설정은 아직 안함.
+	void UpdateProgress(float InProgress, bool InLightOn);
+
+protected:
+	// UMG가 초기화될 때 호출되는 함수.
+	virtual void NativeConstruct() override;
+
+	void SetLight(bool InLight);
+
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<class UProgressBar> Pb_Stun;
+
+private:
+	bool bIsLightOn = false;
+
+};
