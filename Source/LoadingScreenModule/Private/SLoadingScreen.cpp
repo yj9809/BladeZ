fe7e3ce4c10e28c9b2b536c67cce922862653d76
SuYeon <includeashe@gmail.com>
2026-05-28 17:05:03 +0900

    Feat: LoadingScene에서 이미지 비율을 원래 비율대로 가져오도록 설정.

--- Source/LoadingScreenModule/Private/SLoadingScreen.cpp
+++ Source/LoadingScreenModule/Private/SLoadingScreen.cpp
@@ -4,7 +4,9 @@
 #include "SLoadingScreen.h"
 #include "SlateOptMacros.h"
 #include "SlateExtras.h" // For SThrobber
-
+#include "Widgets/Layout/SScaleBox.h"	// For ScaleBox
+#include "Widgets/Layout/SBorder.h"		// For Border
+#include "Styling/CoreStyle.h"
 
 
 BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
@@ -17,26 +19,45 @@ void SLoadingScreen::Construct(const FArguments& InArgs)
 	{
 		BackgroundBrush = MakeShared<FSlateBrush>();
 		BackgroundBrush->SetResourceObject(BackgroundTexture);
+		BackgroundBrush->ImageSize = FVector2D(
+			BackgroundTexture->GetSizeX(),
+			BackgroundTexture->GetSizeY()
+		);
 	}
+
 
 	// Begin constructing the widget hierarchy
 	// ChildSlot is the root slot of our compound widget.
+
 	ChildSlot
 		[
-			// Create an overlay widget which allows stacking multiple elements.
 			SNew(SOverlay)
-				// Add first slot to the overlay for the background image
+
 				+ SOverlay::Slot()
-				.HAlign(HAlign_Fill)	// Stretch horizontally to fill space.
-				.VAlign(VAlign_Fill)	// Stretch vertically to fill space.
+				.HAlign(HAlign_Fill)
+				.VAlign(VAlign_Fill)
 				[
-					// Create an image widget using our background brush
-					SNew(SImage)
-						.Image(BackgroundTexture ? BackgroundBrush.Get():nullptr)
-						.ColorAndOpacity(FLinearColor::White)
+					// Create an overlay widget which allows stacking multiple elements.
+					SNew(SBorder)
+						.BorderImage(FCoreStyle::Get().GetBrush("WhiteBrush"))
+						.BorderBackgroundColor(FLinearColor::Black)
+						[
+							SNew(SScaleBox)
+								.Stretch(EStretch::ScaleToFit)
+								.StretchDirection(EStretchDirection::Both)	// Stretch Both to fill space.
+								.HAlign(HAlign_Center)		// Stretch horizontally to fill space.
+								.VAlign(VAlign_Center)		// Stretch vertically to fill space.
+								[
+									// Create an image widget using our background brush
+									SNew(SImage)
+										.Image(BackgroundTexture ? BackgroundBrush.Get() : nullptr)
+										.ColorAndOpacity(FLinearColor::White)
+								]
+						]
 				]
-				// Add second slot to the overlay for the loading indicator
-				+ SOverlay::Slot()
+
+			// Add second slot to the overlay for the loading indicator
+			+ SOverlay::Slot()
 				.HAlign(HAlign_Right)	// Align to right
 				.VAlign(VAlign_Bottom)	// Align to bottom
 				.Padding(10.0f)			// Add 10 units of padding
@@ -47,6 +68,7 @@ void SLoadingScreen::Construct(const FArguments& InArgs)
 						.NumPieces(10)								// Number of spinning pieces
 				]
 		];
+
 	
 }
 END_SLATE_FUNCTION_BUILD_OPTIMIZATION
